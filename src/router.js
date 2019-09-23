00a70b9e89c88390a604577ca8f765cc41b1e3a1
AlexK <alexjhon@live.com>
2019-09-23 17:32:52 -0500

    Update rooms

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import store from './store';
 import HomePage from './views/HomePage.vue';
 import SearchPage from './views/SearchPage.vue';
 import NotFoundPage from './views/NotFoundPage.vue';
+import CreateHousePage from './views/CreateHousePage.vue';
 
 // User Pages
 import ProfilePage from './views/user/ProfilePage.vue';
@@ -10,7 +12,7 @@ import HousesPages from './views/user/HousesPage.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -32,11 +34,29 @@ export default new Router({
       path: '/user/profile',
       name: 'ProfilePage',
       component: ProfilePage,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: '/user/houses',
       name: 'HousesPages',
       component: HousesPages,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: '/house',
+      redirect: { name: 'ProfilePage' },
+    },
+    {
+      path: '/house/new',
+      name: 'CreateHousePage',
+      component: CreateHousePage,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: '*',
@@ -45,3 +65,17 @@ export default new Router({
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(route => route.meta.requiresAuth)) {
+    if (store.state.authId) {
+      next();
+    } else {
+      next({ name: 'HomePage' });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
